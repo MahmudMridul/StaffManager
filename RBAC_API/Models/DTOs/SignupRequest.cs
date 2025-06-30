2db9588bcfce5ff9aa76d2150131ce6b5334d899
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-06-30 13:46:48 +0600

    Add SignupRequest DTO #1

---
+++ RBAC_API/Models/DTOs/SignupRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RBAC_API.Models.DTOs
+{
+    public class SignupRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long.")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string? LastName { get; set; }
+    }
+}
