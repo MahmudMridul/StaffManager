49c9bd513131be4cb05683efccbb9aee31cdcf9e
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-07-01 08:29:24 +0600

    Update property name #1

--- RBAC_API/Models/User.cs
+++ RBAC_API/Models/User.cs
@@ -11,9 +11,9 @@ namespace RBAC_API.Models
         [MaxLength(100)]
         public string? LastName { get; set; }
 
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime Created { get; set; } = DateTime.UtcNow;
 
-        public DateTime UpdatedAt { get; set; }
+        public DateTime Updated { get; set; }
 
         public bool IsActive { get; set; } = true;
 
