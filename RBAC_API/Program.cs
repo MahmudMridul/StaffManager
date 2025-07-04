4b3ea3bebb7c596c22247dc4942c3fe4fb64dd4f
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-07-04 08:20:45 +0600

    Add Identity, Jwt, CORS configuration #1

--- RBAC_API/Program.cs
+++ RBAC_API/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using RBAC_API.Database;
 using RBAC_API.ExceptionHandler;
+using RBAC_API.Models;
+using System.Text;
 
 namespace RBAC_API
 {
@@ -14,14 +19,69 @@ namespace RBAC_API
 
             builder.Services.AddControllers();
 
-            // Database connection configuration
+            #region Database connection configuration
             builder.Services.AddDbContext<RbacContext>(ops => 
                 ops.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
             );
+            #endregion
 
-            // Global exception handler configuration
+            #region Global exception handler configuration
             builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
             builder.Services.AddProblemDetails(); // additional support for RFC 7807 problem details format
+            #endregion
+
+            #region Identity Configuration
+            builder.Services.AddIdentity<User, Role>(ops =>
+            {
+                ops.User.RequireUniqueEmail = true;
+
+                ops.Password.RequiredLength = 8;
+                ops.Password.RequireDigit = true;
+                ops.Password.RequireUppercase = true;
+                ops.Password.RequireNonAlphanumeric = true;
+                ops.Password.RequiredUniqueChars = 8;
+
+                ops.Lockout.AllowedForNewUsers = true;
+                ops.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                ops.Lockout.MaxFailedAccessAttempts = 5;
+            })
+            .AddEntityFrameworkStores<RbacContext>()
+            .AddDefaultTokenProviders();
+            #endregion
+
+            #region JWT Configuration
+            builder.Services.AddAuthentication(ops =>
+            {
+                ops.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                ops.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(ops =>
+            {
+                ops.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+            });
+            #endregion
+
+            #region CORS
+            builder.Services.AddCors(op =>
+                op.AddPolicy(
+                    "AllowAll",
+                    policy => policy
+                    .WithOrigins("http://localhost:5173")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials()
+                )
+            );
+            #endregion
 
             var app = builder.Build();
 
@@ -31,6 +91,9 @@ namespace RBAC_API
             // Configure the HTTP request pipeline.
 
             app.UseHttpsRedirection();
+
+            // CORS
+            app.UseCors("AllowAll");
 
             app.UseAuthorization();
 
