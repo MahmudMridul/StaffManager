b9cf853976909502ad738d676a3f305f097cd001
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-07-06 17:08:19 +0600

    Remove unwanted properties #1

--- RBAC_API/Models/DTOs/AuthResponse.cs
+++ RBAC_API/Models/DTOs/AuthResponse.cs
@@ -3,9 +3,6 @@ namespace RBAC_API.Models.DTOs
     public class AuthResponse
     {
         public string AccessToken { get; set; } = string.Empty;
-        public string RefreshToken { get; set; } = string.Empty;
-        public DateTime AccessTokenExpiration { get; set; }
-        public DateTime RefreshTokenExpiration { get; set; }
         public UserInfo User { get; set; } = new();
     }
 
